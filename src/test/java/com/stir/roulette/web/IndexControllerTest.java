89e4d769a2e76ac89e3f5c17e3b610b7fc5f0987
junho <wnsgh1357@gmail.com>
2022-11-26 19:45:04 +0900

    test

--- src/test/java/com/stir/roulette/web/IndexControllerTest.java
+++ src/test/java/com/stir/roulette/web/IndexControllerTest.java
@@ -35,36 +35,4 @@ public class IndexControllerTest {
                 .andExpect(content().string(hello));
     }
 
-    @Test
-    public void givenUsingPlainJava_whenGeneratingRandomStringUnbounded_thenCorrect() {
-        int leftLimit = 97; // letter 'a'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
-        Random random = new Random();
-        StringBuilder buffer = new StringBuilder(targetStringLength);
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int)
-                    (random.nextFloat() * (rightLimit - leftLimit + 1));
-            buffer.append((char) randomLimitedInt);
-        }
-        String generatedString = buffer.toString();
-
-        System.out.println(generatedString);
-    }
-
-    @Test
-    public void TTTT(){
-
-
-        String name1 = Optional.of("baeldung").orElse(getName());
-        String name2 = Optional.of("baeldung").orElseGet(() -> getName());
-
-        System.out.println("name1 = " + name1);
-        System.out.println("name2 = " + name2);
-    }
-
-    public String getName() {
-        System.out.println("getName 실행");
-        return "";
-    }
 }
